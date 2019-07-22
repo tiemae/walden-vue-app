d732091bef71f8de394b57264b87f05fcfbd34cf
tiemae <roquerre.t@gmail.com>
2019-07-21 21:18:32 -0700

    changes route of walden for men header

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">Walden.For Men</a>
+            <a class="navbar-brand" href="/">Walden.For Men</a>
           </div>
       
           <div class="collapse navbar-collapse" id="custom-collapse">
